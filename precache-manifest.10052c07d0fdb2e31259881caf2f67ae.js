136386cd7383089d13de681d0eb2adcbadf7c2a8
anaszaman <anaszaman.az@gmail.com>
2021-12-28 02:53:43 +0500

    Updates

---
+++ precache-manifest.10052c07d0fdb2e31259881caf2f67ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d926dcc80fcfdedfae8b440300c2648e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9040ba9530fdc685aae7",
+    "url": "./static/js/2.c6ea22e0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c6ea22e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b54649ec33bf7b2491cf",
+    "url": "./static/js/main.bbba79cc.chunk.js"
+  },
+  {
+    "revision": "ba7abd91aa3d83ef76e7",
+    "url": "./static/js/runtime-main.ecf15d9f.js"
+  }
+]);
